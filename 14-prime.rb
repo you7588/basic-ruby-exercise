c52a6bf67cd88174b955b80e6a5886be14c9ba47
you7588 <yo7588@gmail.com>
2017-08-02 10:17:19 +0800

    15-guess-number.rb

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,22 +1,22 @@
 # 输入一个数字 N，请检查是不是质数
-# 
-# def is_prime(n)
-#   a = 2
-#   while a <= n/2
-#     if n % a == 0
-#   puts "#{a}, #{n}"
-#     end
-#       a = a + 1
-#   end
-#
-#
-# end
-#
-# print "请输入数字 N，然后按 Enter: "
-# n = gets
-#
-# if is_prime(n.to_i)
-#   puts "这是质数"
-# else
-#   puts "这不是质数"
-# end
+
+def is_prime(n)
+  a = 2
+  while a <= n/2
+    if n % a == 0
+      puts "#{a}"
+      break
+    end
+    a = a + 1
+  end
+  n % a !=0
+end
+
+print "请输入数字 N，然后按 Enter: "
+n = gets
+
+if is_prime(n.to_i)
+  puts "这是质数"
+else
+  puts "这不是质数"
+end
